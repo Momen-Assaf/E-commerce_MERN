bfa3e5332cd3f07ea2f67f79cf4c6bceea37fda7
Momen-Assaf <momenssf@gmail.com>
2025-05-01 22:23:35 +0300

    logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Logo from './Logo'
+import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='h-16 shadow-sm'>
+      <div className='h-full containter mx-auto flex items-center px-4 justify-between'>
+        <div className=''>
+          <Logo w={90} h={50} />
+        </div>
+        <div className='flex gap-1 items-center'>
+          <CiSearch fontSize={18} />
+          <input type='text' placeholder='Products Search...'></input>
+        </div>
+      </div>
+    </header>
   )
 }
 
