ec0ba1116602c52fe3a2158197acae4700f6cc1b
Momen-Assaf <momenssf@gmail.com>
2025-05-02 18:46:15 +0300

    search bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from './Logo'
 import { CiSearch } from "react-icons/ci";
+import { FaRegCircleUser } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -9,9 +10,11 @@ const Header = () => {
         <div className=''>
           <Logo w={90} h={50} />
         </div>
-        <div className='flex gap-1 items-center'>
-          <CiSearch fontSize={18} />
-          <input type='text' placeholder='Products Search...'></input>
+        <div className='hidden lg:flex gap-1 items-center justify-between max-w-sm rounded-full focus-within:shadow pl-2'>
+          <input type='text' placeholder='Products Search...' className='w-full outline-none'></input>
+          <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+            <CiSearch fontSize={18} />
+          </div>
         </div>
       </div>
     </header>
