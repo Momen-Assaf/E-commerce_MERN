9951d3684563ca899f12b819c8d3906edcbbc9ea
Momen-Assaf <momenssf@gmail.com>
2025-05-02 19:05:59 +0300

    profile/cart/login-button_header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Logo from './Logo'
 import { CiSearch } from "react-icons/ci";
-import { FaRegCircleUser } from "react-icons/fa6";
+import { FaC, FaRegCircleUser } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -14,6 +15,22 @@ const Header = () => {
           <input type='text' placeholder='Products Search...' className='w-full outline-none'></input>
           <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
             <CiSearch fontSize={18} />
+          </div>
+        </div>
+        <div className='flex items-center gap-4'>
+          <div className='cursor-pointer text-2xl '>
+            <FaRegCircleUser />
+          </div>
+          <div className='relative text-2xl '>
+            <span>
+              <FaCartShopping />
+            </span>
+            <div className='bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-2 -right-3'>
+              <p className='text-sm'>0</p>
+            </div>
+          </div>
+          <div>
+            <button className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</button>
           </div>
         </div>
       </div>
