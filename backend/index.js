cee9e1a92383a0f6b23d136a590223c05f1af365
Momen-Assaf <momenssf@gmail.com>
2025-05-04 22:26:05 +0300

    connection to db

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const cors = require('cors')
 
 require('dotenv').config()
+const connectDB = require('./config/db')
 
 const app = express()
 app.use(cors())
 
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => {
-    console.log('Server is running')
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Connected to DB')
+        console.log('Server is running')
+    })
 })
