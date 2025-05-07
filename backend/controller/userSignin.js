912b86900972b36cbad95323eef00a953639c7e9
Momen-Assaf <momenssf@gmail.com>
2025-05-07 23:56:40 +0300

    jwt and token

--- backend/controller/userSignin.js
+++ backend/controller/userSignin.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/userModels")
 const bycrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 async function userSigninController(req, res) {
     try {
@@ -12,12 +13,22 @@ async function userSigninController(req, res) {
         const isMatch = await bycrypt.compare(password, user.password)
         if (!isMatch) throw new Error('The credentials dont match');
 
-        res.status(200).json({
-            data: user,
+        const tokenData = {
+            _id: user._id,
+            email: user.email,
+        }
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 })
+        const tokenOption = {
+            httpOnly: true,
+            secure: true
+        }
+        res.cookie('token', token, tokenOption).json({
             message: 'Login suceessful',
+            data: token,
             error: false,
             success: true,
         })
+
 
 
     } catch (error) {
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors') // cross origin resourse sharing
 
+const cookieParser = require('cookie-parser')
+
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
@@ -8,6 +10,7 @@ const router = require('./routes')
 const app = express() // the app.use should be before the router
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api', router)
 
