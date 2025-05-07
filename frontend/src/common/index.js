92a4b25775184d68e61323e555c36073187dd7c4
Momen-Assaf <momenssf@gmail.com>
2025-05-07 04:06:11 +0300

    connect back to front

---
+++ frontend/src/common/index.js
@@ -0,0 +1,9 @@
+const backendDomain = 'localhost:8080'
+const SummaryApi = {
+    signUp: {
+        url: `${backendDomain}/api/signup`,
+        method: 'post'
+    }
+}
+
+export default SummaryApi
